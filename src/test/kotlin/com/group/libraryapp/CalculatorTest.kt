160f49b2eb3755d0c7bb836c6ca50aa7155844b9
iyj6707 <iyj6707@naver.com>
2022-09-11 23:57:56 +0900

    feat: initial commit

---
+++ src/test/kotlin/com/group/libraryapp/CalculatorTest.kt
@@ -0,0 +1,55 @@
+package com.group.libraryapp
+
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CalculatorTest {
+
+    @Test
+    fun addTest() {
+        val calculator = Calculator(5)
+
+        calculator.add(3)
+
+        assertThat(calculator.number).isEqualTo(8)
+    }
+
+    @Test
+    fun minusTest() {
+        val calculator = Calculator(5)
+
+        calculator.minus(3)
+
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun multiplyTest() {
+        val calculator = Calculator(5)
+
+        calculator.multiply(3)
+
+        assertThat(calculator.number).isEqualTo(15)
+    }
+
+    @Test
+    fun divideTest() {
+        val calculator = Calculator(5)
+
+        calculator.divide(2)
+
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun divideExceptionTest() {
+        val calculator = Calculator(5)
+
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.apply {
+            assertThat(message).isEqualTo("0으로 나눌 수 없습니다.")
+        }
+    }
+}
